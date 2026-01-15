6026aa99ac5015b5814a2d6f02370839c7a7a75e
Sahil <sahilvrmji@gmail.com>
2026-01-15 15:14:42 +0530

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const index = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-6xl text-red-200">index hello </Text>
+    </View>
+  );
+};
+
+export default index;
