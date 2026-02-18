458794648bb9b6e9eae92416bec5d1bbf015479a
Sahil <sahilvrmji@gmail.com>
2026-02-18 20:40:37 +0530

    ProjectLayoutScreen ui

---
+++ components/TabScreens/projectAboutTabScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProjectAboutTabScreen = () => {
+  return (
+    <View>
+      <Text  style={{
+          color:"white",
+          fontSize:15,
+          letterSpacing:1,
+      }}>ProjectAboutTabScreen</Text>
+    </View>
+  );
+};
+
+export default ProjectAboutTabScreen;
---
+++ components/TabScreens/projectStatsTabScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProjectStatsTabScreen = () => {
+  return (
+    <View>
+      <Text style={{
+          color:"white",
+          fontSize:15,
+          letterSpacing:1,
+      }}>ProjectStatsTabScreen</Text>
+    </View>
+  );
+};
+
+export default ProjectStatsTabScreen;
